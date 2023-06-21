7c1e54ff66920ca6eee07922bc62fd88d6527fda
Derek Stevens <realderekstevens@yahoo.com>
2023-06-20 18:00:12 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+	return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+	return {"data": "This is your posts"}
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+	print(payload)
+	return {"new_post": f"Title: {payload['title']} Content: {payload['content']}"}
