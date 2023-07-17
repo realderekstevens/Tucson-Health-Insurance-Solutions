69e462d0ce9ebf80644113f7117960c464ff13f0
Derek Stevens <realderekstevens@yahoo.com>
2023-07-16 18:45:17 -0700

    1:19:00
    Could put in a way for baby boomers to review their plans and give it a star score on 1 to 5.

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
 class Post(BaseModel):
-	title: str
-	content: str
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional
 
 @app.get("/")
 def root():
-	return {"message": "Hello World"}
+    return {"message": "Hello World"}
 
 @app.get("/posts")
 def get_posts():
-	return {"data": "This is your posts"}
+    return {"data": "This is your posts"}
 
 @app.post("/createposts")
 def create_posts(new_post: Post):
-	print(new_post.title)
-	return {"data": "new post"}
+    print(new_post.published)
+    return {"data": "new post"}
