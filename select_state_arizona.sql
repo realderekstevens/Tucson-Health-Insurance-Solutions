b2cfa97a4a2f0b377a11fda88720c1393ea531fc
Derek <realderekstevens@yahoo.com>
2024-07-21 11:42:15 -0700

    removing redundant sql

--- select_state_arizona.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM Enrollment_2023_06 
-INNER JOIN Contract_2023_06 ON Enrollment_2023_06.contractid_planid = Contract_2023_06.contractid_planid
-WHERE State = 'AZ' 
-ORDER BY enrollment DESC LIMIT 100
