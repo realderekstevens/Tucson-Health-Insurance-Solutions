10bcbdd68ea534edf7488db870d08134dee7ffd3
Derek <72669839+realderekstevens@users.noreply.github.com>
2025-02-21 15:58:32 -0700

    Update simple_import_enrollments.sql

--- sql/bad/simple_import_enrollments.sql
+++ sql/bad/simple_import_enrollments.sql
@@ -14,7 +14,7 @@ CREATE TABLE "CPSC_Contract_Info_2024_12" (
 	OrganizationMarketingName VARCHAR(100),
 	PlanName VARCHAR(100),
 	ParentOrganization,
-	ContractEffectiveDate VARCHAR(100);
+	ContractEffectiveDate VARCHAR(100));
 
 CREATE TABLE "CPSC_Enrollment_Info_2024_12" (
 	ContractID VARCHAR(10),
@@ -22,6 +22,24 @@ CREATE TABLE "CPSC_Enrollment_Info_2024_12" (
 	State VARCHAR(2),
 	County VARCHAR(100),
 	Enrollment INTEGER );
+
+CREATE TABLE "2024_12" (
+	ContractID VARCHAR(10),
+	PlanID INTEGER,
+	OrganizationType VARCHAR(2),
+	PlanType VARCHAR(100),
+	OffersPartD BOOL,
+	SNPPlan BOOL,
+	EGHP BOOL,
+	OrganizationName VARCHAR(100),
+	OrganizationMarketingName VARCHAR(100),
+	PlanName VARCHAR(100),
+	ParentOrganization,
+	ContractEffectiveDate VARCHAR(100);
+	State VARCHAR(2),
+	County VARCHAR(100),
+	Enrollment INTEGER );
+	
 
 COPY "enrollments"(ContractID, PlanID, State, County, Enrollment)
 FROM 'J:\medicare\CPSC_Enrollment_Info_2022_08.csv'
