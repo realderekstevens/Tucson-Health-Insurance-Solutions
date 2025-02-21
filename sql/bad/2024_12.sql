d7de1e79ac691d2454fd99f1bc1920c6ce556adb
Derek <72669839+realderekstevens@users.noreply.github.com>
2025-02-21 16:11:05 -0700

    Update and rename simple_import_enrollments.sql to 2024_12.sql

--- sql/bad/2024_12.sql
+++ sql/bad/2024_12.sql
@@ -17,13 +17,16 @@ CREATE TABLE "CPSC_Contract_Info_2024_12" (
 	ContractEffectiveDate VARCHAR(100));
 
 CREATE TABLE "CPSC_Enrollment_Info_2024_12" (
-	ContractID VARCHAR(10),
+	ContractNumber VARCHAR(10),
 	PlanID INTEGER,
+	SSAStateCountyCode VARCHAR(100),
+	FIPSStateCountyCode VARCHAR(100),
 	State VARCHAR(2),
 	County VARCHAR(100),
 	Enrollment INTEGER );
 
 CREATE TABLE "2024_12" (
+	ID VARCHAR(100),
 	ContractID VARCHAR(10),
 	PlanID INTEGER,
 	OrganizationType VARCHAR(2),
@@ -39,15 +42,19 @@ CREATE TABLE "2024_12" (
 	State VARCHAR(2),
 	County VARCHAR(100),
 	Enrollment INTEGER );
-	
 
-COPY "enrollments"(ContractID, PlanID, State, County, Enrollment)
-FROM 'J:\medicare\CPSC_Enrollment_Info_2022_08.csv'
+COPY "CPSC_Contract_Info_2024_12"(ContractID, PlanID, State, County, Enrollment)
+FROM 'J:\medicare\CPSC_Contract_Info_2024_12.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY "CPSC_Enrollment_Info_2024_12"(ContractID, PlanID, State, County, Enrollment)
+FROM 'J:\medicare\CPSC_Enrollment_Info_2024_12.csv'
 DELIMITER ','
 CSV HEADER;
 
 ALTER TABLE "enrollments" DROP COLUMN ssa;
 ALTER TABLE "enrollments" DROP COLUMN fips;
-ALTER TABLE "enrollments" ADD COLUMN "Date" DATE DEFAULT '2025-02-01' ;
+ALTER TABLE "enrollments" ADD COLUMN "Date" DATE DEFAULT '2024-12-01' ;
 ALTER TABLE "enrollments" ADD COLUMN "ID" text;
 UPDATE "enrollments" SET "ID" = ContractID || '' || PlanID;
